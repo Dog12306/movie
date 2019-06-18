73237146137f2dbd2623bd8563bd1c74cc025f9e
小阿窦 <2250398454@qq.com>
2019-06-18 09:37:07 +0800

    details1

--- src/views/cinema/components/List.vue
+++ src/views/cinema/components/List.vue
@@ -2,12 +2,12 @@
   <div class="list">
     <div class="lit" v-for="list in data">
       <div class="lit-left">
-        <p>{{list.time}}</p>
-        <p>{{list.time1}}散场</p>
+        <p class="left-p1">{{list.time}}</p>
+        <p class="left-p2">{{list.time1}}散场</p>
       </div>
       <div class="lit-cent">
-        <p>{{list.title}}</p>
-        <p>{{list.hall}}</p>
+        <p class="cent-p1">{{list.title}}</p>
+        <p class="cent-p2">{{list.hall}}</p>
       </div>
       <div class="moth">{{list.moth}}元</div>
       <div class="btn">购票</div>
@@ -47,14 +47,58 @@ export default {
   margin: 10px auto;
   display: flex;
   justify-content: space-around;
+  align-items:center;
   .lit-left {
-    p:nth-child(1) {
+    .left-p1 {
       width: 45px;
       height: 25px;
       font-size: 18px;
       line-height: 25px;
       color: white;
+      margin-left: 10px;
     }
+    .left-p2 {
+      width: 60px;
+      height: 17px;
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 17px;
+    }
+  }
+  .lit-cent {
+    .cent-p1 {
+      width: 40px;
+      height: 17px;
+      line-height: 17px;
+      font-size: 12px;
+    }
+    .cent-p2 {
+      width: 68px;
+      height: 34px;
+      font-size: 12px;
+      text-align: left;
+    }
+  }
+  .moth {
+    width: 52px;
+    height: 25px;
+    font-size: 18px;
+    font-weight: 500;
+    color: rgba(251, 195, 74, 1);
+    line-height: 25px;
+  }
+  .btn {
+    width: 45px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    background: linear-gradient(
+      150deg,
+      rgba(242, 91, 134, 1) 0%,
+      rgba(241, 172, 94, 1) 100%
+    );
+    box-shadow: 0px 0px 4px 1px rgba(242, 109, 125, 0.18);
+    border-radius: 6px;
   }
 }
 </style>
